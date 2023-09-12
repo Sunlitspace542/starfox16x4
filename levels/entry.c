b221d316f87a335511c97db81bd67f18a339d95d
sunlitspace542 <rabbidsluis123@gmail.com>
2023-09-12 18:29:06 -0400

    skip title screen and file select

--- levels/entry.c
+++ levels/entry.c
@@ -14,12 +14,12 @@ const LevelScript level_script_entry[] = {
     INIT_LEVEL(),
     SLEEP(/*frames*/ 2),
     BLACKOUT(/*active*/ FALSE),
-#ifdef TEST_LEVEL
-    SET_REG(/*value*/ TEST_LEVEL),
+//#ifdef TEST_LEVEL
+    SET_REG(/*value*/ LEVEL_CASTLE_GROUNDS),
     EXECUTE(/*seg*/ SEGMENT_GLOBAL_LEVEL_SCRIPT, /*script*/ _scriptsSegmentRomStart, /*scriptEnd*/ _scriptsSegmentRomEnd, /*entry*/ level_main_scripts_entry),
-#else
-    SET_REG(/*value*/ 0),
-    EXECUTE(/*seg*/ SEGMENT_MENU_INTRO, /*script*/ _introSegmentRomStart, /*scriptEnd*/ _introSegmentRomEnd, /*entry*/ level_intro_splash_screen),
-#endif
+//#else
+//    SET_REG(/*value*/ 0),
+//    EXECUTE(/*seg*/ SEGMENT_MENU_INTRO, /*script*/ _introSegmentRomStart, /*scriptEnd*/ _introSegmentRomEnd, /*entry*/ level_intro_splash_screen),
+//#endif
     JUMP(/*target*/ level_script_entry),
 };
