af105605f1a8f51713d7e1d6f01d0497eea2822d
sunlitspace542 <rabbidsluis123@gmail.com>
2023-09-17 21:50:36 -0400

    laser strat

--- src/game/behaviors/elaser.inc.c
+++ src/game/behaviors/elaser.inc.c
@@ -1,6 +1,6 @@
 // player's single laser strategy.
 
-// original PATH from SF1 (reference):
+// original PATH and strat from SF1 (reference):
 /*
 fire_Elaser	; player's laser.
 	s_make_obj		#elaser2,.badobj
@@ -12,6 +12,34 @@ fire_Elaser	; player's laser.
 	s_set_colltype		y,friend
 	jsr			gen_weapon
 .badobj	rtl
+*/
+
+/*
+elaser_Istrat 			
+	s_start_strat
+	s_set_strat		x,elaser_strat
+	s_copy_alvar2alvar	B,x,al_rotx,x,al_sbyte1
+	s_copy_alvar2alvar	B,x,al_roty,x,al_sbyte2
+
+	s_gen_3dvecs		x,al_roty,al_rotx,al_vel,1   ; laser spd.
+
+	s_init_anim		x,#0
+	s_end_strat
+elaser_strat
+	s_start_strat
+ 
+	s_cmp_anim		x,#4
+	s_beq			.nai
+	s_add_anim		x,#2,#8
+.nai
+	s_add_vecs2pos		x
+
+	s_decbne_lifecnt	x,.lnd
+	s_remove_obj		x
+	s_end_strat
+.lnd
+	s_brl			miss_end
+
 */
 
 
