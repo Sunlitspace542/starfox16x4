ab39f038290910a0c185840208d05f854bca2b63
sunlitspace542 <rabbidsluis123@gmail.com>
2023-09-17 21:49:27 -0400

    laser strat

--- src/game/behaviors/elaser.inc.c
+++ src/game/behaviors/elaser.inc.c
@@ -1,5 +1,20 @@
 // player's single laser strategy.
 
+// original PATH from SF1 (reference):
+/*
+fire_Elaser	; player's laser.
+	s_make_obj		#elaser2,.badobj
+	s_set_aldata		y,#1,#elaserAP
+	s_set_alptrs		y,Pelaser_Istrat,pelasercollide_Istrat,pelaser2die_Istrat
+	s_set_speed		y,#66
+	s_set_lifecnt		y,#10
+	s_set_colltype		y,laser
+	s_set_colltype		y,friend
+	jsr			gen_weapon
+.badobj	rtl
+*/
+
+
 void elaser_Istrat(void) {
     cur_obj_scale(1.0f);
         o->oVelZ = 66 * 4;
